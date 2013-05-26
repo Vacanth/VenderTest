3daf6452f1f52c448b58ba91322e1ebffc1adc24
Girish Kodaganti <girish.kodaganti@gmail.com>
2013-05-26 12:18:45 -0700

    adding structured projects

---
+++ inventory/src/main/java/com/vendertool/inventory/InventoryManagementServiceImpl.java
@@ -0,0 +1,145 @@
+package com.vendertool.inventory;
+
+import javax.ws.rs.ConsumeMime;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.ProduceMime;
+import javax.ws.rs.QueryParam;
+
+import com.vendertool.common.service.BaseVenderToolServiceImpl;
+import com.vendertool.sharedtypes.core.InventoryErrorCode;
+import com.vendertool.sharedtypes.core.Product;
+import com.vendertool.sharedtypes.core.VTError;
+import com.vendertool.sharedtypes.core.VTErrorDomainEnum;
+import com.vendertool.sharedtypes.core.VTErrorSeverityEnum;
+import com.vendertool.sharedtypes.rnr.AddProductImageRequest;
+import com.vendertool.sharedtypes.rnr.AddProductImageResponse;
+import com.vendertool.sharedtypes.rnr.AddProductRequest;
+import com.vendertool.sharedtypes.rnr.AddProductResponse;
+import com.vendertool.sharedtypes.rnr.AddProductVariationRequest;
+import com.vendertool.sharedtypes.rnr.AddProductVariationResponse;
+import com.vendertool.sharedtypes.rnr.AdjustProductQuantityRequest;
+import com.vendertool.sharedtypes.rnr.AdjustProductQuantityResponse;
+import com.vendertool.sharedtypes.rnr.DuplicateProductResponse;
+import com.vendertool.sharedtypes.rnr.GetProductResponse;
+import com.vendertool.sharedtypes.rnr.GetProductVariationResponse;
+import com.vendertool.sharedtypes.rnr.RemoveProductResponse;
+import com.vendertool.sharedtypes.rnr.RemoveProductVariationResponse;
+import com.vendertool.sharedtypes.rnr.UpdateProductPriceQuanityResponse;
+import com.vendertool.sharedtypes.rnr.UpdateProductPriceQuantityRequest;
+import com.vendertool.sharedtypes.rnr.UpdateProductRequest;
+import com.vendertool.sharedtypes.rnr.UpdateProductResponse;
+
+@Path("/inventory")
+public class InventoryManagementServiceImpl extends BaseVenderToolServiceImpl implements
+		IInventoryManagementService {
+	
+	@GET
+	@Path("/getProduct")
+	@ProduceMime({ "application/xml", "application/json" })
+	public GetProductResponse getProduct(@QueryParam("productId") String id) {
+		GetProductResponse response = new GetProductResponse();
+		Product product = new Product("iPhone 5");
+		String pid = "P123456789";
+		if(id == null) {
+			id = pid;
+		}
+		product.setProductId(id);
+		response.setProduct(product);
+		response.addError(new VTError(
+				InventoryErrorCode.INSUFFICIENT_PRODUCT_INFORMATION,
+				"Insuffient product information", VTErrorSeverityEnum.WARNING,
+				VTErrorDomainEnum.INVENTORY));
+		
+		return response;
+	}
+
+	@POST
+	@Path("/addProduct")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public AddProductResponse addProduct(AddProductRequest request) {
+		Product product = request.getProduct();
+		System.out.println("/inventory/addproduct/ call. Adding product ... " + product.toString());
+		
+		AddProductResponse response = new AddProductResponse();
+		response.setProductId("P987654321");
+		return response;
+	}
+
+	@POST
+	@Path("/updateProduct")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public UpdateProductResponse updateProduct(UpdateProductRequest request) {
+		return null;
+	}
+
+	@DELETE
+	@Path("/removeProduct")
+	@ProduceMime({ "application/xml", "application/json" })
+	public RemoveProductResponse removeProduct(String productId) {
+		return null;
+	}
+
+	@POST
+	@Path("/updateProductPriceQuantity")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public UpdateProductPriceQuanityResponse updateProductPriceQuantity(
+			UpdateProductPriceQuantityRequest request) {
+		return null;
+	}
+
+	@POST
+	@Path("/adjustQuantity")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public AdjustProductQuantityResponse adjustQuantity(
+			AdjustProductQuantityRequest request) {
+		return null;
+	}
+
+	@POST
+	@Path("/duplicateProduct")
+	@ProduceMime({ "application/xml", "application/json" })
+	public DuplicateProductResponse duplicateProduct(String productId) {
+		return null;
+	}
+
+	@POST
+	@Path("/addProductVariation")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public AddProductVariationResponse addProductVariation(
+			AddProductVariationRequest request) {
+		return null;
+	}
+
+	@DELETE
+	@Path("/removeProductVariation")
+	@ProduceMime({ "application/xml", "application/json" })
+	public RemoveProductVariationResponse removeProductVariation(
+			String productId, String variationId) {
+		return null;
+	}
+
+	@GET
+	@Path("/getProductVariation")
+	@ProduceMime({ "application/xml", "application/json" })
+	public GetProductVariationResponse getProductVariation(@QueryParam("productId")String productId,
+			@QueryParam("variationId")String variationId) {
+		return null;
+	}
+
+	@POST
+	@Path("/addProductImage")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public AddProductImageResponse addProductImage(
+			AddProductImageRequest request) {
+		return null;
+	}
+}
---
+++ listing/src/main/java/com/vendertool/listing/ListingServiceimpl.java
@@ -0,0 +1,135 @@
+package com.vendertool.listing;
+
+import javax.ws.rs.ConsumeMime;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.ProduceMime;
+import javax.ws.rs.QueryParam;
+
+import com.vendertool.common.service.BaseVenderToolServiceImpl;
+import com.vendertool.mercadolibreadapter.IMarketListingAdapter;
+import com.vendertool.sharedtypes.core.CountryEnum;
+import com.vendertool.sharedtypes.core.Listing;
+import com.vendertool.sharedtypes.core.MarketEnum;
+import com.vendertool.sharedtypes.rnr.AddListingRequest;
+import com.vendertool.sharedtypes.rnr.AddListingResponse;
+import com.vendertool.sharedtypes.rnr.AdjustListingQuantityRequest;
+import com.vendertool.sharedtypes.rnr.AdjustListingQuantityResponse;
+import com.vendertool.sharedtypes.rnr.DuplicateListingResponse;
+import com.vendertool.sharedtypes.rnr.EndListingResponse;
+import com.vendertool.sharedtypes.rnr.GetAuctionBidCountResponse;
+import com.vendertool.sharedtypes.rnr.GetAuctionHighBidderResponse;
+import com.vendertool.sharedtypes.rnr.GetListingPriceQuantityResponse;
+import com.vendertool.sharedtypes.rnr.GetListingResponse;
+import com.vendertool.sharedtypes.rnr.UpdateListingPriceQuanityResponse;
+import com.vendertool.sharedtypes.rnr.UpdateListingPriceQuantityRequest;
+import com.vendertool.sharedtypes.rnr.UpdateListingRequest;
+import com.vendertool.sharedtypes.rnr.UpdateListingResponse;
+import com.vendertool.sharedtypes.rnr.VerifyListingRequest;
+import com.vendertool.sharedtypes.rnr.VerifyListingResponse;
+
+@Path("/1.0/listing")
+public class ListingServiceimpl extends BaseVenderToolServiceImpl 
+	implements IListingService {
+	
+
+	@GET
+	@Path("/getListing")
+	@ProduceMime({ "application/xml", "application/json" })
+	public GetListingResponse getListing(@QueryParam("listingId") String id) {
+		CountryEnum countryId = CountryEnum.ALL;
+		MarketEnum marketId = MarketEnum.MERCADO_LIBRE;
+		
+		IMarketListingAdapter adapter = MarketListingRegistry.getInstance().getMarketListingAdapter(
+				new MarketCountryKey(countryId, marketId));
+		
+		return adapter.getListing(id);
+	}
+	
+	@POST
+	@Path("/addListing")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public AddListingResponse addListing(AddListingRequest request) {
+		Listing listing = request.getListing();
+		IMarketListingAdapter adapter = MarketListingRegistry.getInstance().getMarketListingAdapter(
+				new MarketCountryKey(listing.getCountry(), listing.getMarket()));
+		adapter.addListing(request);
+				
+		AddListingResponse response = new AddListingResponse();
+		response.setListingId("L987654321");
+		
+		return response;
+	}
+
+	@GET
+	@Path("/getListingPriceQuantity")
+	@ProduceMime({ "application/xml", "application/json" })
+	public GetListingPriceQuantityResponse getListingPriceQuantity(
+			String listingId) {
+		return null;
+	}
+
+	@GET
+	@Path("/endListing")
+	@ProduceMime({ "application/xml", "application/json" })
+	public EndListingResponse endListing(String listingId) {
+		return null;
+	}
+
+	@POST
+	@Path("/updateListing")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public UpdateListingResponse updateListing(UpdateListingRequest request) {
+		return null;
+	}
+
+	@POST
+	@Path("/updateListingPriceQuantity")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public UpdateListingPriceQuanityResponse updateListingPriceQuantity(
+			UpdateListingPriceQuantityRequest request) {
+		return null;
+	}
+
+	@POST
+	@Path("/adjustListingQuantity")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public AdjustListingQuantityResponse adjustListingQuantity(
+			AdjustListingQuantityRequest request) {
+		return null;
+	}
+
+	@GET
+	@Path("/duplicateListing")
+	@ProduceMime({ "application/xml", "application/json" })
+	public DuplicateListingResponse duplicateListing(String listingId) {
+		return null;
+	}
+
+	@GET
+	@Path("/getAuctionBidCount")
+	@ProduceMime({ "application/xml", "application/json" })
+	public GetAuctionBidCountResponse getAuctionBidCount(String listingId) {
+		return null;
+	}
+
+	@GET
+	@Path("/getAuctionHighBidder")
+	@ProduceMime({ "application/xml", "application/json" })
+	public GetAuctionHighBidderResponse getAuctionHighBidder(String listingId) {
+		return null;
+	}
+
+	@POST
+	@Path("/verifyListing")
+	@ConsumeMime({ "application/xml", "application/json" })
+	@ProduceMime({ "application/xml", "application/json" })
+	public VerifyListingResponse verifyListing(VerifyListingRequest request) {
+		return null;
+	}
+}
